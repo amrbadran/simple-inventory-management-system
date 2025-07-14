ee5e0541ae0f62c2d672a170e83249fb36287cbd
amrbadran <badran.amr2003@gmail.com>
2025-07-14 20:25:48 +0300

    Edit Product Feature

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello Simple Inventory Management System");
+﻿using Simple_Inventory_Management_System.logic;
+
+// Sample Test for Editing and Viewing Products...
+
+Console.WriteLine("Hello Simple Inventory Management System");
+
+var ops = new InventoryOperations();
+
+ops.AddProduct("Milk",15.5,3);
+ops.AddProduct("Rice",20,5);
+
+var list = ops.ViewProducts();
+
+foreach (string message in list)
+{
+    Console.WriteLine(message);
+}
+
+ops.EditProduct(ops.ProductExists("Milk"),"Milk1",15.5,3);
