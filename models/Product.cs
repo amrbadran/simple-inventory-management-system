bf8f32d39d4ada0f3409091de898a8a162de8866
amrbadran <badran.amr2003@gmail.com>
2025-07-14 09:24:47 +0300

    Add Product Class

---
+++ models/Product.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Simple_Inventory_Management_System.models;
+
+public class Product
+{
+    [SetsRequiredMembers]
+    public Product(string name, double price, int quantity)
+    {
+        this.Name = name;
+        this.Price = price;
+        this.Quantity = quantity;
+    }
+    public required string Name { get; set; }
+    public required double Price { get; set; }
+    public required int Quantity { get; set; }
+}
